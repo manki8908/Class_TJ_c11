654670451a62ec862235ba71e225f4c0d9f035b5
kmk <aksrl132@gmail.com>
2023-05-02 12:36:58 +0900

    1st

--- DJANGO/project1/mysite/pybo/views/base_views.py
+++ DJANGO/project1/mysite/pybo/views/base_views.py
@@ -8,6 +8,15 @@ def index(request):
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
     question_list = Question.objects.order_by('-create_date')
+    so = request.GET.get('so', 'recent')
+
+    if so == 'recommend': 
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_voter', '-create_date')
+    else:
+        question_list = Question.objects.annotate(num_voter=Count('answer')).order_by('-num_voter', '-create_date')
+
     if kw:
         question_list = question_list.filter(
             Q(subject__icontains=kw) |  # 제목 검색
@@ -16,9 +25,10 @@ def index(request):
             Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
             Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
         ).distinct()
+
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
--- DJANGO/project2/testsite/test_app/views.py
+++ DJANGO/project2/testsite/test_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
